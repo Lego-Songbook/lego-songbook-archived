49ed01f70647f15e082d81ffa730961885c4da5f
Kip Yin <28321392+kipyin@users.noreply.github.com>
2019-10-16 23:45:12 +0800

    Finish `add_worship()`

--- sing.py
+++ sing.py
@@ -1,10 +1,16 @@
 from datetime import date
-from typing import Sequence, Tuple
+from typing import Sequence, Tuple, Mapping
 
-from peewee import *
+from peewee import (
+    CharField,
+    DateField,
+    IntegerField,
+    ManyToManyField,
+    SqliteDatabase,
+    Model,
+)
 
-
-db = SqliteDatabase("songbook.db")
+db = SqliteDatabase(None)
 
 
 class BaseModel(Model):
@@ -14,15 +20,21 @@ class BaseModel(Model):
 
 class Song(BaseModel):
 
-    name = CharField()
+    name = CharField(unique=True)
     key = CharField()
-    hymn_ref = IntegerField()
+    hymn_ref = IntegerField(null=True)
+
+    def __repr__(self):
+        return f"Song(id={self.id}, name={self.name}, key={self.key}, hymn_ref={self.hymn_ref})"
 
 
 class Arrangement(BaseModel):
 
     name = CharField()
     role = CharField()
+
+    def __repr__(self):
+        return f"Arrangement(id={self.id}, role={self.role}, name={self.name})"
 
 
 class Worship(BaseModel):
@@ -31,83 +43,71 @@ class Worship(BaseModel):
     songs = ManyToManyField(Song)
     arrangements = ManyToManyField(Arrangement)
 
-
-# worship = Worship.create(date=date(2019, 10, 6))
-# song1 = Song.get(Song.name == 'song1')
-# song2 = Song.get(Song.name == 'song2')
-# arr1 = Arrangement.get(Arrangement.role='keyboard', Arrangement.name='Adam')
-# worship.songs.add([song1, song2])
-# worship.arrangements.add([arr1])
+    def __repr__(self):
+        return f"Worship(id={self.id}, date={self.date}, songs={[song for song in self.songs]}, arrangements={[arr for arr in self.arrangements]})"
 
 
 WorshipSong = Worship.songs.get_through_model()
 WorshipArrangement = Worship.arrangements.get_through_model()
 
 
-# class WorshipSong(BaseModel):
-#     worship = ForeignKeyField(Worship)
-#     song = ForeignKeyField(Song)
+def initialize(db_uri):
+    db.init(db_uri)
+    db.create_tables([Song, Arrangement, Worship, WorshipSong, WorshipArrangement])
+
+
+# def add_song(name: str, key: str, hymn_ref: int = None) -> bool:
+#     """Add a song into the database.
 #
+#     :param name: the name of the song
+#     :param key: the key of the song
+#     :param hymn_ref: the index number of the song in Hymn
 #
-# class WorshipArrangement(BaseModel):
+#     Example:
+#     >>> add_song(name="Song A", key="Bb", hymn_ref=253)
+#     True
 #
-#     worship = ForeignKeyField(Worship)
-#     arrangement = ForeignKeyField(Arrangement)
+#     """
+#     Song.create(name=name, key=key, hymn_ref=hymn_ref)
 
 
-db.create_tables([Song, Arrangement, Worship, WorshipSong, WorshipArrangement])
-
-
-def add_song(name: str, key: str, hymn_ref: int = 0) -> bool:
-    """Add a song into the database.
-
-    :param name: the name of the song
-    :param key: the key of the song
-    :param hymn_ref: the index number of the song in Hymn
-
-    Example:
-    >>> add_song(name="Song A", key="Bb", hymn_ref=253)
-    True
-
-    """
-    Song.create(name=name, key=key, hymn_ref=hymn_ref)
-
-
-def add_songs(songs: Sequence[Tuple]) -> bool:
+def add_songs(songs: Sequence[Tuple[str, str, int]]) -> bool:
     """Bulk insert songs into the database."""
 
     with db.atomic():
-        Song.insert_many(songs, fields=[Song.name, Song.key, Song.hymn_ref]).execute()
+        Song.insert_many(songs, fields=[Song.name, Song.key, Song.hymn_ref]
+            ).on_conflict(
+                conflict_target=[Song.name],
+                preserve=[Song.key, Song.hymn_ref],
+            ).execute()
+# 
+#
+# def add_arrangements(arrangements: Mapping[str, str]) -> bool:
+#     """Add an arrangement into the database.
+#
+#     :param arrangements: a dictionary of role: name pairs
+#
+#     Example:
+#     >>> add_arrangement({"Leader": "Adam", "Keyboard": "Bella"})
+#     True
+#
+#     """
+#     # arrangement_data = [(role, name) for role, name in arrangements.items()]
+#     with db.atomic():
+#         Arrangement.insert_many(
+#             arrangements.items(), fields=[Arrangement.role, Arrangement.name]
+#         ).on_conflict_ignore().execute()
 
 
-def add_arrangement(date: date, **arrangements) -> bool:
-    """Add an arrangement into the database.
-
-    :param date: the service's date
-    :param arrangement: a list of arrangement tuple
-
-    Example:
-    >>> add_arrangement(
-    ...     date='2019-10-06',
-    ...     arrangement=[('lead', 'Adam'), ('keyboard', 'Bella')],
-    ... )
-    True
-
-    """
-    arrangement_data = [(date, role, name) for role, name in arrangements]
-    with db.atomic():
-        Arrangement.insert_many(
-            arrangement_data,
-            fields=[Arrangement.date, Arrangement.role, Arrangement.name]
-        ).execute()
-
-
-def add_worship(date: date, songs: Sequence[str], **arrangements) -> bool:
+def add_worship(
+    date: date, songs: Sequence[str], arrangements: Mapping[str, str]
+) -> bool:
     """Add a service into the database."""
     worship = Worship.create(date=date)
-    arrangement_data = [(date, role, name, worship) for role, name in arrangements]
-    with db.atomic():
-        Arrangement.insert_many(
-            arrangement_data,
-            fields=[Arrangement.date, Arrangement.role, Arrangement.name, Arrangement.worship]
-        ).execute()
+    # song_list = Song.select().where(Song.name.in_(songs)).execute()
+    arrangement_list = []
+    for role, name in arrangements.items():
+        arrangement_list.append(Arrangement.get_or_create(role=role, name=name)[0])
+
+    worship.songs.add(Song.select().where(Song.name.in_(songs)))
+    worship.arrangements.add(arrangement_list)
