33fa345dfe2f3fe75310f1fc2c84b517d799dee0
Kip Yin <28321392+kipyin@users.noreply.github.com>
2019-10-27 00:27:51 +0800

    Make `console` into a module

---
+++ src/songbook/console/__init__.py
@@ -0,0 +1,45 @@
+"""
+This module defines the command line behaviors.
+
+Intended usage:
+
+    $ songbook init  # builds the database from csv files.
+    $ songbook sync  # make sure the db and the csv files are the same.
+
+    $ songbook import songs --format folder --input docs/assets/sheets/
+    $ songbook export songs -f csv -o docs/_data/songs.csv [--update | --ignore]
+
+    $ songbook config --db-uri db/songbook.db
+
+    $ songbook view songs --limit 10 --key F
+    $ songbook view songs --name "Song A"  # fuzzy match
+    $ songbook view songs --date 2019-10-27
+
+    $ songbook view worship --date 2019-10-27
+    $ songbook view worship --recent 10
+    $ songbook view worship --next-week
+
+    $ songbook view arr --date 2019-10-27  # arrangements only
+
+    $ songbook add song --name "Song A" --key "F#m" --hymn_ref 123
+    $ songbook remove song --name "Song A"
+    $ songbook remove song --key "C#" --all
+    $ songbook update song --
+
+"""
+import os
+
+import click
+
+from .. import models
+from ._import import import_  # lol
+from ._view import view
+
+
+@click.group()
+def main():
+    models.init(os.environ.get("SONGBOOK_DB", "db/songbook.db"))
+
+
+main.add_command(import_)
+main.add_command(view)
--- src/songbook/console/_import.py
+++ src/songbook/console/_import.py
@@ -1,43 +1,8 @@
-"""
-This module defines the command line behaviors.
-
-Intended usage:
-
-    $ songbook init  # builds the database from csv files.
-    $ songbook sync  # make sure the db and the csv files are the same.
-
-    $ songbook import songs --format folder --input docs/assets/sheets/
-    $ songbook export songs -f csv -o docs/_data/songs.csv [--update | --ignore]
-
-    $ songbook config --db-uri db/songbook.db
-
-    $ songbook view songs --limit 10 --key F
-    $ songbook view songs --name "Song A"  # fuzzy match
-    $ songbook view songs --date 2019-10-27
-
-    $ songbook view worship --date 2019-10-27
-    $ songbook view worship --recent 10
-    $ songbook view worship --next-week
-
-    $ songbook view arr --date 2019-10-27  # arrangements only
-
-    $ songbook add song --name "Song A" --key "F#m" --hymn_ref 123
-    $ songbook remove song --name "Song A"
-    $ songbook remove song --key "C#" --all
-    $ songbook update song --
-
-"""
-
 import os
 
 import click
 
-from . import models
-
-
-@click.group()
-def main():
-    models.init("db/songbook.db")
+from .. import models
 
 
 def _import_songs_from_folder(input_, delimiter, fields, conflict_action):
@@ -55,11 +20,11 @@ def _import_songs_from_folder(input_, delimiter, fields, conflict_action):
     )
 
 
-def _import_songs_from_file(format_, input_):
+def _import_songs_from_file(input_, format_):
     pass
 
 
-def _import_songs(format_, input_, delimiter, fields, conflict_action):
+def _import_songs(input_, format_, delimiter, fields, conflict_action):
     if format_ == "folder":
         _import_songs_from_folder(
             input_=input_,
@@ -68,7 +33,7 @@ def _import_songs(format_, input_, delimiter, fields, conflict_action):
             conflict_action=conflict_action,
         )
     else:
-        _import_songs_from_file(format_, input_)
+        _import_songs_from_file(input_, format_)
 
 
 def _import_arrangements():
@@ -79,7 +44,7 @@ def _import_worships():
     pass
 
 
-@main.command("import")
+@click.command("import")
 @click.argument("table")
 @click.option("-f", "--format", "format_")
 @click.option("-i", "--input", "input_")
@@ -90,8 +55,8 @@ def _import_worships():
 def import_(table, format_, input_, delimiter, fields, conflict_action):
     if table == "songs":
         _import_songs(
-            format_=format_,
             input_=input_,
+            format_=format_,
             delimiter=delimiter,
             fields=fields,
             conflict_action=conflict_action,
@@ -102,28 +67,3 @@ def import_(table, format_, input_, delimiter, fields, conflict_action):
         _import_worships()
     else:
         raise ValueError(f"Invalid table name {table}.")
-
-
-@main.command()
-def export():
-    pass
-
-
-@main.command()
-def config():
-    pass
-
-
-@main.command()
-def view():
-    pass
-
-
-@main.command()
-def sync():
-    pass
-
-
-@main.command()
-def init():
-    pass
