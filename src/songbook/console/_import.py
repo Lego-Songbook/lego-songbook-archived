3965d224d942e68820030b3f3f51dc7e477a4979
Kip Yin <28321392+kipyin@users.noreply.github.com>
2019-10-27 23:25:38 +0800

    Refactor `songbook import` command into a group

--- src/songbook/console/_import.py
+++ src/songbook/console/_import.py
@@ -10,25 +10,9 @@ from ..models import Song, Arrangement, Worship, Hymn
 _MODEL_NAMES = {
     "song": Song,
     "arrangement": Arrangement,
-    "arr": Arrangement,
     "worship": Worship,
     "hymn": Hymn,
 }
-
-
-def _import_songs_from_folder(input_, delimiter, fields, conflict_action) -> int:
-    song_data = []
-    for sheet_file in os.scandir(input_):
-        song_data.append(sheet_file.name.split(delimiter)[: len(fields)])
-    return (
-        Song.insert_many(song_data, fields=(Song.key, Song.name))  # TODO: Use str
-        .on_conflict(
-            action=conflict_action,
-            conflict_target=[Song.name],
-            preserve=[Song.name],
-        )
-        .execute()
-    )
 
 
 def _import_from_file(table, input_) -> int:
@@ -37,26 +21,50 @@ def _import_from_file(table, input_) -> int:
     return model.insert_many(data.dict).execute()
 
 
-@click.command("import")
-@click.argument("table")
-@click.option("-i", "--input", "input_")
+@click.group("import")
+def import_():
+    pass
+
+
+# Importing songs from a dir seems like a one-off effort.
+# Maybe I should just remove this for the sake of simplicity.
+@import_.command("song")
+@click.option("-i", "--input", "input_", required=True)
 @click.option("-d", "--delimiter", default="-")
 @click.option("--fields", default=(Song.key, Song.name))
 @click.option("--update", "conflict_action", flag_value="update", default=True)
 @click.option("--ignore", "conflict_action", flag_value="ignore")
-def import_(table, input_, delimiter, fields, conflict_action):
-    if table not in _MODEL_NAMES.keys():
-        raise ValueError(f"Invalid table name: {table}.")
-    if input_ is None:
-        raise ValueError("Must specify a directory or a path to import via `--input`.")
-    elif table == "song" and input_ is not None and Path(input_).is_dir():
-        result = _import_songs_from_folder(
-            input_=input_,
-            delimiter=delimiter,
-            fields=fields,
-            conflict_action=conflict_action,
+def _import_song(input_, delimiter, fields, conflict_action):
+    if Path(input_).is_dir():
+        song_data = []
+        for sheet_file in os.scandir(input_):
+            song_data.append(sheet_file.name.split(delimiter)[: len(fields)])
+        return (
+            Song.insert_many(song_data, fields=(Song.key, Song.name))  # TODO: Use str
+                .on_conflict(
+                action=conflict_action,
+                conflict_target=[Song.name],
+                preserve=[Song.name],
+            )
+            .execute()
         )
     else:
-        return _import_from_file(table=table, input_=input_)
+        return _import_from_file(table="song", input_=input_)
 
-    print(result)
+
+@import_.command("arrangement")
+@click.option("-i", "--input", "input_", required=True)
+def _import_arrangement(input_):
+    _import_from_file(table="arrangement", input_=input_)
+
+
+@import_.command("worship")
+@click.option("-i", "--input", "input_", required=True)
+def _import_arrangement(input_):
+    _import_from_file(table="worship", input_=input_)
+
+
+@import_.command("hymn")
+@click.option("-i", "--input", "input_", required=True)
+def _import_arrangement(input_):
+    _import_from_file(table="hymn", input_=input_)
