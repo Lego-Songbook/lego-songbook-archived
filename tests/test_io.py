dc147c905318d053b303db04a95874d44dec2cb5
Kip Yin <28321392+kipyin@users.noreply.github.com>
2019-11-05 00:01:40 +0800

    Keep only the useful functions and tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,6 +1,9 @@
+from io import StringIO
+
 import pytest
 
-from songbook import io, models
+import songbook.io
+from songbook import models
 
 ARRANGEMENT_CSV = """\
 id,person_id,role_id
@@ -17,4 +20,21 @@ id,name
 
 @pytest.mark.usefixtures("test_clean_db")
 def test_load():
-    assert io._load(table=models.Artist, in_stream=ARTIST_CSV, format_="csv") == 2
+    songbook.io.load(table=models.Artist, in_stream=StringIO(ARTIST_CSV), format_="csv")
+
+    artist = models.Artist.get_by_id(2)
+    assert artist.name == "Artist 2"
+
+
+@pytest.mark.usefixtures("test_db")
+def test_dump():
+    data = songbook.io.dump(models.Artist, "csv")
+    assert (
+        data
+        == """\
+id,name\r
+1,Artist 1\r
+2,Artist 2\r
+3,Artist 3\r
+"""
+    )
